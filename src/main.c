1e5ff0cb7029a44bb8bfee33a24e63aa02e701e2
adzierzanowski <a.dzierzanowski1@gmail.com>
2020-02-27 22:09:22 +0100

    implement delay based on systick irq

--- src/main.c
+++ src/main.c
@@ -1,8 +1,18 @@
 #include "stm32f031x6.h"
 
 
+volatile uint32_t ticks = 0;
+
 void sys_tick_handler(void)
 {
+  ticks++;
+}
+
+void delay_ms(uint32_t ms)
+{
+  volatile uint32_t start = ticks;
+  while (ticks - start < ms)
+    __NOP();
   return;
 }
 
@@ -13,11 +23,14 @@ int main(void)
   GPIOB->MODER |= (0b01 << 3 * 2);
   GPIOB->OTYPER &= ~(1 << 3);
 
+  // no need to enable irq as apparently it's enabled by default
+  SysTick_Config(8000000 / 1000);
+
   for(;;)
   {
     GPIOB->BSRR = 1 << 3;
-    for (int i = 0; i < 80000; i++);
+    delay_ms(100);
     GPIOB->BRR = 1 << 3;
-    for (int i = 0; i < 80000; i++);
+    delay_ms(100);
   }
 }
